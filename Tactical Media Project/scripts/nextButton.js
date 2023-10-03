f2e2ee8b680567e91f16af70d8f6d31f3432d70e
Zach Marino <zachmarino234@gmail.com>
2023-10-03 18:25:36 -0400

    Added tactical media project

--- Tactical Media Project/scripts/nextButton.js
+++ Tactical Media Project/scripts/nextButton.js
@@ -1,18 +1,23 @@
 function getCurrentURL() {
-    return window.location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+  return window.location.pathname.substring(
+    location.pathname.lastIndexOf("/") + 1
+  );
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const btn = document.getElementById("rectangle-btn");
   btn.addEventListener("click", function () {
     if (getCurrentURL() === "about.html") {
-        window.location.href = "blog.html";
-    }
-    else if (getCurrentURL() === "blog.html") {
-        window.location.href = "yahoo.html";
-    }
-    else if (getCurrentURL() === "yahoo.html") {
-        window.location.href = "index.html";
+      window.location.href = "blog.html";
+
+    } else if (getCurrentURL() === "blog.html") {
+      window.location.href = "yahoo.html";
+
+    } else if (getCurrentURL() === "yahoo.html") {
+      window.location.href = "index.html";
+
+    } else if (getCurrentURL() === "home.html") {
+      window.location.href = "../../Personal Website/pages/code.html";
     }
   });
 });
